651118a75de75945983108dc248632f144e57c53
bensandison <56243297+bensandison@users.noreply.github.com>
2021-03-31 15:48:07 +0100

    added text to map, still need to correctly position text

--- sketch.js
+++ sketch.js
@@ -8,23 +8,31 @@ function preload(){
   data = loadJSON(weatherUrl);
 
   let mapKey = 'pk.eyJ1IjoiYmVuLXNhbmRpc29uIiwiYSI6ImNrbXdzcmZoazBpNzkydm52bXk4dG5wNW0ifQ.qZTYDoEcLJSjYow5OWIqqQ';
-  let mapUrl = 'https://api.mapbox.com/styles/v1/mapbox/streets-v11/static/[' + lonLeft + ',' + latBottom + ',' + lonRight + ',' + latTop + ']/1000x800?access_token=' + mapKey;
+  let mapUrl = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/[' + lonLeft + ',' + latBottom + ',' + lonRight + ',' + latTop + ']/800x600?access_token=' + mapKey;
   mapImg = loadImage(mapUrl);
+
+
+  // console logs:
+  print(weatherUrl);
+  print(mapUrl);
 }
 
 function setup() {
-  createCanvas(1000, 800);
-
-  for(i=0; i<data.list.length; i++){
-    print(data.list[i].name)
-    print(data.list[i].main.temp)
-  }
+  createCanvas(800, 600);
 
 }
 
 function draw() {
   background(220);
   
-  image(mapImg, 0, 0, 1000, 800);
+  image(mapImg, 0, 0, 800, 600);
+
+  for(i=0; i<data.list.length; i++){
+    x = map(data.list[i].coord.Lon, -6, 3, 0, 800);
+    y = map(data.list[i].coord.Lat, 51.5, 49.8, 0, 600);
+
+    fill(255, 255, 255);
+    text(data.list[i].name, x, y);
+  }
 
 }

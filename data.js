2ebd68505575d036744c9738f97205f3cd39a23a
bensandison <56243297+bensandison@users.noreply.github.com>
2021-04-12 20:24:54 +0100

    moved code into data file and cleaned up api calls

---
+++ data.js
@@ -0,0 +1,46 @@
+function loadMap(){		/* constructs mapBox api uri and loads map as an image */
+	mapKey = "pk.eyJ1IjoiYmVuLXNhbmRpc29uIiwiYSI6ImNrbXdzb25ucDBpNTMyb252NWUxZDJpMzAifQ.VihZeh8XW1EXJ3HDCTi6Yg"
+	styleUrl = "ben-sandison/cknesa8u13c4k17l4pzc6xzlc"		/* url for my custom styling */
+	mapUrl = "https://api.mapbox.com/styles/v1/" + styleUrl + "/static/" + mapData.lonMid + "," + mapData.latMid + "," + mapData.mapZoom + ",0/" + mapData.mapX + "x" + mapData.mapY + "?access_token=" + mapKey;
+	
+	mapImg = loadImage(mapUrl);		// load map to "mapImage"
+}
+
+function loadWeather(){		/* constructs openWeatherMaps api uri and loads data */
+	let weatherKey = '5e48243f4e2c1794ffeb1248ec1503a7';   //api key
+	let lonLeft = '-6', latBottom = '49.8', lonRight = '3', latTop  = '51.5', weatherZoom = '10';  //API call bounding box coords
+	let weatherUrl = 'http://api.openweathermap.org/data/2.5/box/city?bbox=' + lonLeft + ',' + latBottom + ',' + lonRight + ',' + latTop + ',' + weatherZoom + '&appid=' + weatherKey + '&units=metric';
+
+	data = loadJSON(weatherUrl);	//load weather data to "data" object
+}
+
+function calculateCoords(){		/* Calculates X and Y position of cities */
+	//These functions are used to convert Longitude and Latitude to X and Y positions
+	//They use 'Mercator projection'
+	function mercX(lon) {
+		lon = radians(lon);
+		let a = (256 / PI) * pow(2, mapData.mapZoom);
+		let b = lon + PI;
+		return a * b;
+	}
+	function mercY(lat){
+		lat = radians(lat);
+		let a = (256 / PI) * pow(2, mapData.mapZoom);
+		let b = tan(PI/4 + lat/2);
+		let c = PI - log(b);
+		return a * c;
+	}
+
+	//These variables give us the central position of our map
+	let cx = mercX(mapData.lonMid);
+	let cy = mercY(mapData.latMid);
+
+	//We then loop through and add the X and Y positions as properties of the data object
+	for(let i=0; i<data.list.length; i++){
+		data.list[i].xPos = mercX(data.list[i].coord.Lon) - cx;	//calculate x position
+		data.list[i].yPos = mercY(data.list[i].coord.Lat) - cy;
+	}
+}
+
+
+
--- sketch.js
+++ sketch.js
@@ -1,75 +1,25 @@
 let data;
 let mapImg;
-let lonMid = '-2', latMid = '51', mapZoom = '6.2';  //central lat/lon and zoom level for map
+const mapData = {	//data about map coordinates
+	lonMid : -2,
+	latMid : 51,
+	mapZoom : 6.2,
+	mapX : 800,
+	mapY : 600,
+}
 
 function preload(){
-	//weather api
-	let weatherKey = '5e48243f4e2c1794ffeb1248ec1503a7';   //api key
-	let lonLeft = '-6', latBottom = '49.8', lonRight = '3', latTop  = '51.5', weatherZoom = '10';  //API call bounding box coords
-	let weatherUrl = 'http://api.openweathermap.org/data/2.5/box/city?bbox=' + lonLeft + ',' + latBottom + ',' + lonRight + ',' + latTop + ',' + weatherZoom + '&appid=' + weatherKey + '&units=metric';
-	data = loadJSON(weatherUrl);
-
-
-	let mapKey = 'pk.eyJ1IjoiYmVuLXNhbmRpc29uIiwiYSI6ImNrbXdzcmZoazBpNzkydm52bXk4dG5wNW0ifQ.qZTYDoEcLJSjYow5OWIqqQ';
-	//lonMid, latMid and mapZoom are declared above
-	let mapUrl = 'https://api.mapbox.com/styles/v1/mapbox/dark-v9/static/' + lonMid + ',' + latMid + ',' + mapZoom + ',0/800x400?access_token=' + mapKey;
-	mapImg = loadImage(mapUrl);
-
-
-	// console logs:
-	print(weatherUrl);
-	print(mapUrl);
+	loadMap();
+	loadWeather();
 }
 
 function setup() {
-	createCanvas(800, 600);
-	background(220);
+	createCanvas(mapData.mapX, mapData.mapY);
+	background(255);
 	translate(width / 2, height/2);
 	imageMode(CENTER);
 
-	//draw map image
-	image(mapImg, 0, 0);
+	calculateCoords();	//calculate x and y positions of cities
 
-	// let lon = -1, lat = 51.5;
-
-	let cx = mercX(lonMid);
-	let cy = mercY(latMid);
-
-	// let x = mercX(lon) - cx;
-	// let y = mercY(lat) - cy;
-
-	// fill(255, 0, 255, 200);
-	// ellipse(x, y, 20, 20);
-
-	for(i=0; i<data.list.length; i++){
-		x = mercX(data.list[i].coord.Lon) - cx;
-		y = mercY(data.list[i].coord.Lat) - cy;
-
-		//draw place name
-		fill(255, 255, 255);
-		text(data.list[i].name, x, y);
-
-		let radius = map(data.list[i].main.temp, 5, 30, 30, 70);
-		let red = map(data.list[i].main.temp, 5, 10, 50, 255);
-
-		//draw temp circle
-		fill(red, 200, 0, 50);
-		noStroke();
-		ellipse(x, y, radius);
-	}
-}
-
-function mercX(lon) {	/* Takes a longitude and translates to an x position on the map (using the mercator projection) */
-	lon = radians(lon);
-	let a = (256 / PI) * pow(2, mapZoom);
-	let b = lon + PI;
-	return a * b;
-}
-
-function mercY(lat){	/* Takes lat and translates to Y pos*/
-	lat = radians(lat);
-	let a = (256 / PI) * pow(2, mapZoom);
-	let b = tan(PI/4 + lat/2);
-	let c = PI - log(b);
-	return a * c;
+	image(mapImg, 0, 0);	//Draws map image
 }
